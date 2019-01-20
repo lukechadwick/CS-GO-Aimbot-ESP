bc751729f05bd7db4b9a63d84dd1fd511286837e
lukechadwick <llukechadwick@gmail.com>
2019-01-20 17:33:15 +1300

    config

---
+++ libSodium/libSodium/Config.cs
@@ -0,0 +1,15 @@
+﻿namespace libSodium
+{
+    internal class Config
+    {
+        public static int FOV = 100;
+        public static int aimSpeed = 4;
+        public static int boneSelector = 10;
+        public static int numberOfPlayers = 64;
+        public static int threadPerformance = 16;
+        public static float sensitivity = 3f;
+        public static bool active = false;
+        public static bool focused = false;
+        public static string windowTitle;
+    }
+}
