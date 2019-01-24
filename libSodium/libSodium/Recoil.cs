0448d3068c09b01dc31b7bf32815e4d1ba2e6fde
lukechadwick <llukechadwick@gmail.com>
2019-01-25 12:29:12 +1300

    Recoil

---
+++ libSodium/libSodium/Recoil.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace libSodium
+{
+    internal class Recoil
+    {
+        public static void compensation()
+        {
+            Vector3 Punch = Memory.ReadVec3(vars.localClass.LocalPlayer + Offsets.m_viewPunchAngle);
+            vars.recoilX = (int)((vars.ScreenWidth / 2) - (vars.ScreenWidth / 90) * (Punch.Y * Config.sensitivity)); // These values are used for the aimbot to offset recoil punch , the +1 is to correct it to the center
+            vars.recoilY = (int)((vars.ScreenHeight / 2) + (vars.ScreenHeight / 90) * (Punch.X * Config.sensitivity));
+        }
+    }
+}
